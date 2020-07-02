33be9f26767f25b45fe469a3243e86d9fa480c63
ITO Daisuke <ito@wacul.co.jp>
2020-07-02 19:29:46 +0900

    thread count default 10

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def init():
     service_parser.add_argument('--task-definition-config-env', default=True, action='store_true')
     service_parser.add_argument('--no-task-definition-config-env', dest='task_definition_config_env', default=True,
                                 action='store_false')
-    service_parser.add_argument('--threads-count', type=int, default=3)
+    service_parser.add_argument('--threads-count', type=int, default=10)
     service_parser.add_argument('--service-wait-max-attempts', type=int, default=72)
     service_parser.add_argument('--service-wait-delay', type=int, default=10)
     service_parser.add_argument('--service-zero-keep', dest='service_zero_keep', default=True, action='store_true')
